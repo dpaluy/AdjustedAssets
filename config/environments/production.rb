942adc5f2a686c2d150b7ab7179c1b741cf5e162
David P <david@paluy.org>
2011-12-31 22:16:04 +0200

    heroku production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,8 @@ AdjustedAssets::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
